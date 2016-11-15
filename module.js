2996c156ff29a54003c8956f87814edfb79d2f05
Dan Riding <doonyby@gmail.com>
2016-11-14 17:26:03 -0700

    added controller as

--- module.js
+++ module.js
@@ -2,4 +2,6 @@ angular.module('myApp', [])
 	.constant('SOMETHING', 'I dont get it')
 	.controller('myCtrl', function(SOMETHING, $scope) {
 		$scope.something = SOMETHING;
+		var vm = this;
+		vm.title = 'What did I do with the constant?'
 	});
