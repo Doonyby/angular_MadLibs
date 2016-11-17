3cbb796ed5f85a01d2fdfd5760dfe5d4f3711984
Dan Riding <doonyby@gmail.com>
2016-11-17 11:51:38 -0700

    completed styling and logic

--- module.js
+++ module.js
@@ -1,7 +1,32 @@
-angular.module('myApp', [])
-	.constant('SOMETHING', 'I dont get it')
-	.controller('myCtrl', function(SOMETHING, $scope) {
-		$scope.something = SOMETHING;
+angular.module('myApp', ['ngMessages'])
+	.controller('myCtrl', function() {
 		var vm = this;
-		vm.title = 'What did I do with the constant?'
+		vm.screenSwitch = false;
+		vm.redBox = false;
+		vm.submit = function() {
+			if( vm.myForm.$valid ) {
+				console.log('form is valid');
+			    vm.screenSwitch = true;
+			} else {
+				console.log('form is not valid');
+				vm.redBox = true;
+			    vm.screenSwith = false;
+			}
+		};
+		vm.resetMadLib = function() {
+			vm.myForm.$setPristine();
+			console.log('resetting mad lib');
+			vm.screenSwitch = false;
+			vm.redBox = false;
+			vm.femaleName = '';
+			vm.jobTitle = '';
+			vm.tediousTask = '';
+			vm.dirtyTask = '';
+			vm.celebrity = '';
+			vm.uselessSkill = '';
+			vm.obnoxious = '';
+			vm.hugeNumber = '';
+			vm.adjective = '';
+		};
+
 	});
